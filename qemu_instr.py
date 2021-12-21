a68845c6020a1920fa8a196d5587c57a7c0986e9
shouc <scf@ieee.org>
2021-12-20 17:44:37 -0800

    qemu instr add comp

--- qemu_instr.py
+++ qemu_instr.py
@@ -1,6 +1,10 @@
+import time
+
 import instr_interface
 import pwn
 import config
+import os
+
 
 class STDINExecutorQEMU:
     def __init__(self, qemu_bin, uninstrumented_path, instrumented_path):
@@ -40,6 +44,24 @@ class QEMUInstr(instr_interface.Instrumentation):
         self.dumper_path = dumper_path
         self.shm_key = shm_key
         self.visited_trace = set()
+        self.__non_comp_bb = set()
+        self.__grab_non_comp_bb()
+
+    # QEMU would dump all BBs, even those call / jmp, we have to know this!
+    def __grab_non_comp_bb(self):
+        floc = f"/tmp/objdump-log-{time.time()}"
+        os.system(f"objdump -d {self.executor.uninstrumented_path} > {floc}")
+        record_next = False
+        for line in open(floc).read().split("\n"):
+            line_arr = line.split("\t")
+            if record_next:
+                # ['', '', '', '', '1265:\t66', '66', '2e', '0f', '1f', '84', '00', '\tdata16', 'cs', 'nopw', '0x0(%rax,%rax,1)']
+                if len(line_arr) > 5:
+                    pc = int("0x" + line_arr[5].split(":")[0])
+                    self.__non_comp_bb.add(pc)
+                record_next = False
+            if "\tcall" in line_arr or "\tjmp" in line_arr:
+                record_next = True
 
     def __add_to_execution_tree(self, trace, file_name):
         last_node = None
@@ -47,6 +69,8 @@ class QEMUInstr(instr_interface.Instrumentation):
             if addr not in self.execution_tree:
                 self.execution_tree[addr] = instr_interface.Node()
                 self.execution_tree[addr].addr = addr
+                if addr not in self.__non_comp_bb:
+                    self.execution_tree[addr].is_comp = True
             current_node = self.execution_tree[addr]
             if last_node is not None and (last_node.left != current_node and last_node.right != current_node):
                 if last_node.left is None:
@@ -76,7 +100,8 @@ class QEMUInstr(instr_interface.Instrumentation):
         for line in content.split("\n"):
             line_arr = line.split(",")
             pc, counter = line_arr[0], line_arr[1]
-            self.execution_tree[pc].visit_count = counter
+            if pc in self.execution_tree:
+                self.execution_tree[pc].visit_count = counter
 
     def call_dumper(self):
         return pwn.process(self.dumper_path, env={
